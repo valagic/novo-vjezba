07e316c18c1e2d2b9e4e9532b29a55a68d2126bc
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-02 22:28:21 +0200

    vjezbanje zadatka 1

--- src/novovjezba/NovoVjezba.java
+++ src/novovjezba/NovoVjezba.java
@@ -3,6 +3,11 @@ package novovjezba;
 public class NovoVjezba {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		// deklarirajte varijablu za pohranu tjelesne
+		// temperature èovjeka kao int i dodjelite 
+		// vrijednost 36
+		
+		int tjelesnaTemperature = 36;
+		System.out.println(tjelesnaTemperature);
 	}
 }
