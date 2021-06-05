a2ce39649365a20f1260fd8c14366a54463f25d7
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-05 21:34:08 +0200

    vjezbanje

--- src/novovjezba/VjezbanjeSintakse2.java
+++ src/novovjezba/VjezbanjeSintakse2.java
@@ -1,11 +1,55 @@
 package novovjezba;
 
+import javax.swing.JOptionPane;
+
 public class VjezbanjeSintakse2 {
 
 	public static void main(String[] args) {
 		
 		int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,r,s,t,v,z;
 		
+		a = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+		b = Integer.parseInt(JOptionPane.showInputDialog("unesi drugi broj"));
+		c = Integer.parseInt(JOptionPane.showInputDialog("unesi treci broj"));
+		d = Integer.parseInt(JOptionPane.showInputDialog("unesi 4ti broj"));
+		e = Integer.parseInt(JOptionPane.showInputDialog("unesi 5ti broj"));
+		f = Integer.parseInt(JOptionPane.showInputDialog("unesi 6ti broj"));
+		g = Integer.parseInt(JOptionPane.showInputDialog("unesi 7mi broj"));
+		h = Integer.parseInt(JOptionPane.showInputDialog("unesi 8mi broj"));
+		i = Integer.parseInt(JOptionPane.showInputDialog("unesi 9ti broj"));
+		j = Integer.parseInt(JOptionPane.showInputDialog("unesi 11ti broj"));
+		k = Integer.parseInt(JOptionPane.showInputDialog("unesi 12ti broj"));
+		l = Integer.parseInt(JOptionPane.showInputDialog("unesi 13ti broj"));
+		m = Integer.parseInt(JOptionPane.showInputDialog("unesi 14ti broj"));
+		n = Integer.parseInt(JOptionPane.showInputDialog("unesi 15ti broj"));
+		o = Integer.parseInt(JOptionPane.showInputDialog("unesi 16ti broj"));
+		p = Integer.parseInt(JOptionPane.showInputDialog("unesi 17ti broj"));
+		r = Integer.parseInt(JOptionPane.showInputDialog("unesi 18ti broj"));
+		s = Integer.parseInt(JOptionPane.showInputDialog("unesi 19ti broj"));
+		t = Integer.parseInt(JOptionPane.showInputDialog("unesi 20ti broj"));
+		v = Integer.parseInt(JOptionPane.showInputDialog("unesi 21 broj"));
+		z = Integer.parseInt(JOptionPane.showInputDialog("unesi 21 broj"));
 		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(o);
+		System.out.println(p);
+		System.out.println(r);
+		System.out.println(s);
+		System.out.println(t);
+		System.out.println(v);
+		System.out.println(z);
 	}
 }
