18b80c0732a96c9ccd96201a5e83f833505a6f0f
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-07 23:06:10 +0200

    dodavanje zadataka

--- src/novovjezba/VjezbaZadaci.java
+++ src/novovjezba/VjezbaZadaci.java
@@ -4,5 +4,91 @@ public class VjezbaZadaci {
 
 	public static void main(String[] args) {
 		
+		/*Zadatak 1
+		korisnik unosi cijeli broj
+		za parni broj se ispisuje Osijek
+		za neparni broj se ispisuje Donji Miholjac*/
+		
+		
+		
+		
+		
+		
+		/*dodatno zadatak 1
+		U sluèaju unosa parnog broja tražiti unos
+		dva cijela broja i ispisati zbroj*/
+		
+		
+		
+		
+		
+		
+		
+		/*u sluèaju unosa neparnog broja tražiti unos
+		decimalnog broja i ispisati samo cijeli dio/*
+
+		
+		
+		
+		/*Zadatak 2
+		Program unosi dva cijela broja od korisnika
+		program ispisuje veæi uneseni broj*/
+		
+		// jedno od rješenja
+		/*
+		public static void main(String[] args) {
+			if(Integer.parseInt(
+					JOptionPane.showInputDialog("Prvi broj"))>
+			Integer.parseInt(JOptionPane.showInputDialog("Drugi broj"))) {
+				System.out.println("Prvi je veæi");
+			}else {
+				System.out.println("Drugi je veæi");
+			}
+		}
+		*/
+		
+		
+		
+		
+		
+		
+		/*Zadatak 3
+		Program unosi tri cijela broja od korisnika
+		program ispisuje najmanji uneseni broj*/
+		
+		/*Zadatak 4 - primitivni kalkulator
+		Korisnika unosi prvo operaciju:
+		1 - zbrajanje
+		2 - oduzimanje
+		3 - množenje
+		4 - djeljenje
+		za sve ostale unose program ispisuje: GREŠKA i završava se
+		Korisnik unosi dva decimalna broja
+		U odnosu na odabranu operaciju program ispisuje rezultat
+		Ako je rezultat cijeli broj tada se ispisuje bez .0*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		/*Zadatak 5
+		Program unosi dva cijela broja.
+		U sluèaju da je zbroj unesenih brojeva veæi od 10 
+		i razlika prvog i drugog manja od 0 program ispisuje
+		Osijek, inaèe ispisuje Donji Miholjac.*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
