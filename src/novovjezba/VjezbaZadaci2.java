3329f391c0cd8779e2c6383051f3e74fa1b9439d
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-14 23:34:58 +0200

    zadaci za vjezbanje - samo zadaci

---
+++ src/novovjezba/VjezbaZadaci2.java
@@ -0,0 +1,39 @@
+package novovjezba;
+
+public class VjezbaZadaci2 {
+
+	public static void main(String[] args) {
+		
+		
+		//Program uèitava 5 decimalnih brojeva u niz
+		//Program ispisuje prvi i posljednji element niza
+		
+		//Program preko args parametara prima dva cijela broja
+		//Program ispisuje zbroj primljenih brojeva
+		
+		//Korisnik unosi cijeli broj
+		//Program ispisuje broj 7 jedno ispod drugog
+		//onoliko puta koliko je korisnik unio
+		
+		// program ispisuje sve parne brojeve od 1 do 55
+		// U petlji obavezno koristiti vrijednosti 1 i 55
+		
+		/*
+		//loše rješenje
+		public static void main(String[] args) {
+			for(int i=2;i<=54;i+=2) {
+				System.out.println(i);
+			}
+		}
+		*/
+		
+		
+		//Program unosi 5 cijelih brojeva u niz
+		//Program ispisuje najveæi uneseni broj
+		
+		
+		
+		
+		
+	}
+}
