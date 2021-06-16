55bd15176e79893438a24dc329d48213429bcd78
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-16 10:40:23 +0200

    zadatak5

--- src/novovjezba/VjezbaZadaci2.java
+++ src/novovjezba/VjezbaZadaci2.java
@@ -1,5 +1,7 @@
 package novovjezba;
 
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class VjezbaZadaci2 {
@@ -10,7 +12,7 @@ public class VjezbaZadaci2 {
 		//Program uèitava 5 decimalnih brojeva u niz
 		//Program ispisuje prvi i posljednji element niza
 		
-		double a,b,c,d,e;
+		/*double a,b,c,d,e;
 		
 		a = Double.parseDouble(JOptionPane.showInputDialog("unesi prvi decimalni broj"));
 		b = Double.parseDouble(JOptionPane.showInputDialog("unesi drugi decimalni broj"));
@@ -19,7 +21,7 @@ public class VjezbaZadaci2 {
 		e = Double.parseDouble(JOptionPane.showInputDialog("unesi peti decimalni broj"));
 		
 		double niz[] = {a,b,c,d,e};
-		System.out.println(niz[0]+ " " +niz[4]);
+		System.out.println(niz[0]+ " " +niz[4]);*/
 		
 		
 		
@@ -37,13 +39,13 @@ public class VjezbaZadaci2 {
 		//Program ispisuje broj 7 jedno ispod drugog
 		//onoliko puta koliko je korisnik unio
 		
-		int g;
+		/*int g;
 		
 		g = Integer.parseInt(JOptionPane.showInputDialog("unesi jedan cijeli broj"));
 		
 		for(int i=0;i<g;i++) {
 			System.out.println("ispisi broj 7");
-		}
+		}*/
 		
 		
 		
@@ -52,13 +54,13 @@ public class VjezbaZadaci2 {
 		// U petlji obavezno koristiti vrijednosti 1 i 55
 		
 		
-		for(int i=1;i<=55;i++) {
+		/*for(int i=1;i<=55;i++) {
 			if(i%2==0) {
 				System.out.println(i);
 			}else {
 				System.out.println("nije parni broj");
 			}
-		}
+		}*/
 		
 		
 		
@@ -76,9 +78,23 @@ public class VjezbaZadaci2 {
 		//Program unosi 5 cijelih brojeva u niz
 		//Program ispisuje najveæi uneseni broj
 		
+		int prviBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi drugi broj"));
+		int treciBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi treci broj"));
+		int cetvrtiBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi cetvrti broj"));
+		int petiBroj = Integer.parseInt(JOptionPane.showInputDialog("unesi peti broj"));
 		
+		int[] niz = {prviBroj,drugiBroj,treciBroj,cetvrtiBroj,petiBroj};
+		int najveciBroj = 0;
 		
+		System.out.println(Arrays.toString(niz));
 		
+		for(int i=0;i<niz.length;i++) {
+			if(niz[i] > najveciBroj) {
+				najveciBroj = niz[i];
+			}
+			
+		}System.out.println(najveciBroj);
 		
 	}
 }
