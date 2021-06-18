d47c571cdd222bf0c409dfe1bfe431fa969668f3
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-18 13:34:08 +0200

    vjezbanje zadataka

--- src/novovjezba/VjezbaZadaci3.java
+++ src/novovjezba/VjezbaZadaci3.java
@@ -1,5 +1,7 @@
 package novovjezba;
 
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class VjezbaZadaci3 {
@@ -32,8 +34,23 @@ public class VjezbaZadaci3 {
 		// Program ispisuje sve brojeve od 100 do 21
 		// jedno do drugoga odvojeno zarezom
 		
+		for(int i=100;i>=21;i--) {
+			System.out.println(i);
+		}
+		
 		// Program uèitava cijele brojeve od korisnika
 		// sve dok se ne uèita broj 0. Tada se program završava
+		int c;
+	
+		
+		for(;;) {
+			c = Integer.parseInt(JOptionPane.showInputDialog("unesi broj, ako uneses 0 program ce se prekinuti."));
+			if(c==0) {
+				break;
+			}
+				
+			
+		}
 		
 		// Program od korisnika unosi dva parna broja
 		// Program ispisuje njohov zbroj
@@ -41,6 +58,33 @@ public class VjezbaZadaci3 {
 		// Program unosi 10 cijelih brojeva od korisnika
 		// u jednodimenzionalni niz
 		// Program ispisuje sve elemente niza
+		
+						/* jedan od nacina koji nije dobar
+						int c,d,e,f,g,h,i,j,k,l;
+				
+						c = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						d = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						e = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						f = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						g = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						h = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						i = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						j = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						k = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						l = Integer.parseInt(JOptionPane.showInputDialog("unesi prvi broj"));
+						
+						int[] niz = {c,d,e,f,g,h,i,j,k,l};
+						
+						System.out.println(Arrays.toString(niz));*/
+
+		
+		int niz[] = new int[10];
+		
+		for(int i=0;i<niz.length;i++) {
+			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("unesi broj"));
+		}
+		System.out.println(Arrays.toString(niz));
+		
 		
 		// Prim broj je broj koji je cjelobrojno djeljiv sam s sobom
 		// i s broj 1.
@@ -59,8 +103,6 @@ public class VjezbaZadaci3 {
 		// djeljenja
 		
 		// Program ispisuje "Edunova" (ispisuju se i navodnici)
-		
-		
 		
 	}
 }
